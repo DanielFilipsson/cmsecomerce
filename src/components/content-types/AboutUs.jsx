b84fb3a711a0a6deb29c45b114ae6ba6d3455b9d
Elin Östman <elin.ostman@hotmail.com>
2024-09-11 15:02:13 +0200

    tog bort en kommentar

--- src/components/content-types/AboutUs.jsx
+++ src/components/content-types/AboutUs.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function AboutUs({ blok }) {
   return (
     <section className="flex flex-col items-center justify-center w-full min-h-screen p-8 space-y-8 text-center">
-      <div className="max-w-3xl mx-auto"> {/* Maxbredd satt till 3xl för att begränsa textens bredd */}
+      <div className="max-w-3xl mx-auto">
         <h1 className="text-5xl md:text-6xl font-extrabold text-black">
           {blok.title || 'Om Oss'}
         </h1>
