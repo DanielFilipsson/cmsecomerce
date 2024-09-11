cb871145f1bf3691a7bfb10d30110a94b60f716c
Elin Östman <elin.ostman@hotmail.com>
2024-09-11 12:33:44 +0200

    back to home-button tillagt

--- src/components/content-types/AboutUs.jsx
+++ src/components/content-types/AboutUs.jsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export default function AboutUs({ blok }) {
   return (
     <section className="flex flex-col items-center justify-center w-full min-h-screen p-8 space-y-8 text-center">
@@ -5,9 +7,12 @@ export default function AboutUs({ blok }) {
         <h1 className="text-5xl md:text-6xl font-extrabold text-black">
           {blok.title || 'Om Oss'}
         </h1>
-        <p className="text-lg md:text-xl text-gray-700 mt-4">
+        <p className="text-lg md:text-xl text-gray-700 mt-4 mb-6">
           {blok.text || 'Standard Om Oss-text visas här.'}
         </p>
+        <Link href={blok.link.cached_url || '#'}>
+            {blok.link.title || 'Home'}
+        </Link>
       </div>
     </section>
   );
